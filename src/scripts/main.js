5af29b77a2f6c3c7259053f0caf29e863742088d
juankparis <juan.k.paris@gmail.com>
2016-02-29 20:21:36 -0500

    interactividad de boton buscar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,23 @@
 var $ = require('jquery');
-
+	
 	var $Menu = $('#Menu'),
 		$menuClick = $('#menuClick'),
 		$Headerbuscar = $("#Header-buscar");
+		$abrirBuscar = $("#abrirBuscar");
+		$from = $("#from");
 
 (function(){
-	
+	//menu
 	$menuClick.on('click', menuAbrir);
 
 	function menuAbrir(){
 		$Menu.toggleClass("U-toggleMenu");
+	}
+	//barra de buscar
+	$abrirBuscar.on('click', abrirBuscar);
+
+	function abrirBuscar() {
+		$from.toggleClass("U-toggleBuscar");
 	}
 
 }());

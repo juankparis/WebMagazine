f2e6b480380bc61252077218e2b7e241a096de5b
juankparis <juan.k.paris@gmail.com>
2016-03-09 01:44:43 -0500

    se agrego el inicio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,25 @@
 var $ = require('jquery');
 	
-	var $Menu = $('#Menu'),
+	var $Header = $('#Header'),
+		$Menu = $('#Menu'),
+		$inicio = $('#inicio'),
+		$home = $('#home'),
+		$creditos = $('#creditos'),
 		$menuClick = $('#menuClick'),
 		$Headerbuscar = $("#Header-buscar"),
 		$abrirBuscar = $("#abrirBuscar"),
 		$from = $("#from"),
 		$Leer = $("#leer"),
+		$Cont = $("#Cont"),
+		$ingresar = $("#ingresar"),
 		$ContInfo = $("#ContInfo"),
 		$ContInfoMovie = $(".ContInfo-movie"),
 		$ContInfoAtras = $("#ContInfo-atras");
 
 (function(){
 
-	var ajax = require('./lib/ajax');
-	ajax();
+	// var ajax = require('./lib/ajax');
+	// ajax();
 
 	//menu
 	$menuClick.on('click', menuAbrir);
@@ -29,149 +35,79 @@ var $ = require('jquery');
 			.find("input").toggleClass("U-toggleWidth");
 	}
 
+	$ingresar.on('click', entrar);
+	$home.on('click', entrar); 
 
-	setTimeout(function() {
-
-		$Leer.on('click', leerArticle);
-
-		function leerArticle(){
-			event.preventDefault();
-			$ContInfo.toggleClass("U-expandirArticleMovie");
-				setTimeout(
+	function entrar(){
+		$Cont.find(".Cont-inicio").addClass("U-borderEntrar");
+		$Cont.find(".Cont-inicioHeader").addClass("U-displayNone");
+		$Header.removeClass("U-z-index")
+			setTimeout(
 					function() {
-						$ContInfo.toggleClass("U-expandirArticleMovie2");
-						$ContInfoMovie.fadeIn("slow");
+						$Header.removeClass("U-creditos");
+						$Headerbuscar.removeClass("U-visibility");
+						$Cont.find(".Cont-inicioInfo").fadeOut();
+						$Cont.find(".Cont-inicio").addClass("U-widhtCero");
+						$Cont.find(".Cont-inicioLogo").addClass("U-logo");
 					}, 300);
-		}
-
-		$ContInfoAtras.on('click', cerrarArticle);
-
-		function cerrarArticle() {
-			$ContInfo.toggleClass("U-expandirArticleMovie2");
-			$ContInfoMovie.fadeOut("slow");
-				setTimeout(
-					function(){
-						$ContInfo.toggleClass("U-expandirArticleMovie");
-					}, 300);
-		}
-	},1000);
-/*
-/////////////////////shows //////////////
-
-	var $Cont = $("#Cont");
-
-	function renderShows(shows) {
-		shows.forEach(function(show){
-			var article =template
-				.replace(':name:', show.name)
-				.replace(':img:', show.image.medium)
-				.replace(':img alt:', show.name + " logo")
-				.replace(':language:', show.language);
-
-			$Cont.append($(article));
-			// $(article).appendTo($Cont);
-
-			var article2 =template2
-				.replace(':name:', show.name)
-				.replace(':img:', show.image.original)
-				.replace(':img alt:', show.name + " logo")
-				.replace(':language:', show.language)
-				.replace(':genres:', show.genres)
-				.replace(':status:', show.status)
-				.replace(':Scheduled:', show.schedule.days + ' at time: '+ show.schedule.time)
-				.replace(':timezone:', show.network.country.timezone)
-				.replace(':summary:', show.summary)
-				.replace(':weight:', show.weight)
-				.replace(':rating:', show.rating.average);
-
-			$Cont.append($(article2));
-			// $(article2).appendTo($Cont);
-		})
 	}
-	// function renderShowslib(shows) {
-	// 	shows.forEach(function (show) {
-	// 	})
-	// }
-// buscar shows
-	$Headerbuscar.find("form").submit(function(ev){
-		ev.preventDefault();
-		// alert("me hizieron click");
-		var busqueda = $(this)
-			.find('input[type="text"]')
-			.val();
-			// alert("buscaron " + busqueda);
-			$Cont.find('.Cont-article').remove();
-			var $loader = $('<div class="loader"></div>')
-			$loader.appendTo($Cont);
-		$.ajax({
-			url: 'http://api.tvmaze.com/search/shows',
-			data:{ q: busqueda },
-			success: function (res, textStatus, xhr) {
-				// console.log(res);
-				$loader.remove();
-				var shows = res.map(function(el){
-					return el.show;
-				})
-				renderShows(shows);
-			}
-		})
-	});
 
-// template
-	var template = '<article class="Cont-article">' +
-			'<figure class="Cont-articleFigure">' +
-				'<img src=":img:" alt=":img alt:" width="80" height="100">' +
-			'</figure>' +
-			'<div class="Cont-articleInfo">' +
-				'<h2 class="Cont-articleInfoTitle">:name:</h2>' +
-				'<h4 class="Cont-articleInfoAutor">:language:</h4>' +
-				'<a class="Cont-articleInfoButton icon-plus" id="leer">Leer</a>' +
-			'</div>' +
-		'</article>';
+	$inicio.on('click', inicio);
+	
+	function inicio(){
+		$Cont.find(".Cont-inicioInfo").fadeIn();
+		$Cont.find(".Cont-inicio").removeClass("U-widhtCero");
+		$Cont.find(".Cont-inicioLogo").removeClass("U-logo");
+		$Header.removeClass("U-z-index");
+			setTimeout(
+					function () {
+						$Header.removeClass("U-creditos");
+						$Headerbuscar.removeClass("U-visibility");
+						$Cont.find(".Cont-inicio").removeClass("U-borderEntrar");
+						$Cont.find(".Cont-inicioHeader").removeClass("U-displayNone");	
+					},300);
+	}
 
-	var template2='<article class="ContInfo" id="ContInfo">' +
-			'<div class="ContInfo-movie">' +
-				'<img src=":img:" alt=":img alt:" width="50" height="50">' +
-				'<div class="ContInfo-movieInfo">' +
-					'<h2 class="Cont-articleInfoTitle">:name:</h2>' +
-					'<h4 class="Cont-articleInfoAutor">:language:</h4>' +
-					'<h4 class="Cont-articleInfoAutor"><span>genres: </span>:genres:</h4>' +
-					'<h4 class="Cont-articleInfoAutor"><span>Status: </span>:status:</h4>' +
-					'<h4 class="Cont-articleInfoAutor"><span>Scheduled: </span>:Scheduled:</h4>' +
-					'<h4 class="Cont-articleInfoAutor"><span>timezone: </span>:timezone:</h4>' +
-					'<h4 class="Cont-articleInfoAutor"><span>Official site:</span> <a href="#">www.cwtv.com</a></h4>' +
-					'<div class="ContInfo-movieInfoSumary">' +
-						'<p>:summary:</p>' +
-					'</div>' +
-					'<h4 class="ContInfo-movieInfoVotes"><span>Votes:</span>:weight:</h4>' +
-					'<h4 class="ContInfo-movieInfoStars icon-star">:rating:</h4>' +
-				'</div>' +
-				'<div class="ContInfo-atras icon-collapse" id="ContInfo-atras"></div>' +
-			'</div>' +
-		'</article>';
+	$Cont.find(".Header-iconMenu").on('click',mensaje)
+	
+	function mensaje() {
+		alert("ingresa primero y conoce nuestros servicios");
+	}
 
-// shows
-	$.ajax({
-		url: 'http://api.tvmaze.com/shows',
-		success: function(shows, textStatus, xhr){
-			console.log(shows);
-			// var $Cont = $("#Cont");
-			$Cont.find('.loader').remove();
-			// shows.forEach(function(show){
-			// 	var article =template
-			// 		.replace(':name:', show.name)
-			// 		.replace(':img:', show.image.medium)
-			// 		.replace(':img alt:', show.name + " logo")
-			// 		.replace(':language:', show.language);
+	$creditos.on('click', creditos);
+	
+	function creditos(){
+		$Header.addClass("U-creditos");
+		$Headerbuscar.addClass("U-visibility");
+		$Cont.find(".Cont-inicio").removeClass("U-widhtCero");
+		$Cont.find(".Cont-inicioLogo").addClass("U-logo2");
+		setTimeout(
+			function(){
+				$Cont.find(".Cont-inicio").removeClass("U-borderEntrar");
+			},300);
+	}
 
-			// 	$Cont.append($(article));
-			// })
-			renderShows(shows);
-		}
-	})
+	$Leer.on('click', leerArticle);
 
-	// $(".Cont-article").live('click', function () {
-	// 	alert("me hizieron click");
-	// });
-*/
+	function leerArticle(event){
+		event.preventDefault();
+		$ContInfo.toggleClass("U-expandirArticleMovie");
+			setTimeout(
+				function() {
+					$ContInfo.toggleClass("U-expandirArticleMovie2");
+					$ContInfoMovie.fadeIn("slow");
+				}, 300);
+	}
+
+	$ContInfoAtras.on('click', cerrarArticle);
+
+	function cerrarArticle() {
+		$ContInfo.toggleClass("U-expandirArticleMovie2");
+		$ContInfoMovie.fadeOut("slow");
+			setTimeout(
+				function(){
+					$ContInfo.toggleClass("U-expandirArticleMovie");
+				}, 300);
+	}
+
 }());
